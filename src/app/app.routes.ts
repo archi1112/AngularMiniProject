b50967cda577bdfc2629c0cd15b624b0a107c4f1
agolia <goliaarchi@gmail.com>
2024-05-29 12:15:16 +0530

    invalid route 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  //   this is wildcard for 404 page , invalid route
+  // {path:"**" , component:PageNotFoundComponent}
 ];
