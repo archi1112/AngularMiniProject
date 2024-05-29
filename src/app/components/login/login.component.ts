c0edbe464113bb9e8cffae4e85cd24aca159720d
agolia <goliaarchi@gmail.com>
2024-05-29 12:08:57 +0530

    implemented template and reactive forms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  //  implementing forms
+  // forms are 2 types : reactive and template
+  // forms - reactice forms logic is in typescript file
+  // while template form have logic in html file
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+
+  loginForm = new FormGroup({
+    email: this.email,
+    password: this.password,
+  });
+
+  login() {
+    console.log(this.loginForm.value);
+  }
+  reset() {
+    this.loginForm.reset();
+  }
 }
